e2b6370962a954483decc4bfe07b801dc9cc1aab
Oleg Medvedev <omdv@fastmail.com>
2021-10-10 20:21:54 -0500

    obfuscated domain name

--- docker-ibgateway/runscript.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-/opt/IBController/Scripts/DisplayBannerAndLaunch.sh &
-sleep 1
-tail -f $(find $LOG_PATH -maxdepth 1 -type f -printf "%T@ %p\n" | sort -n | tail -n 1 | cut -d' ' -f 2-) &
-
-jupyter lab --ip=0.0.0.0 --allow-root
--- k3s-setup.sh
+++ k3s-setup.sh
@@ -4,6 +4,8 @@ kubectl apply -k kubernetes/general
 helm repo add bitnami https://charts.bitnami.com/bitnami
 helm install metrics bitnami/kube-prometheus --values=kubernetes/charts/prometheus-chart-values.yaml -n kube-system
 
+kubectl apply -k kubernetest/traefik
+
 helm repo add traefik https://helm.traefik.io/traefik
 helm install traefik traefik/traefik --values=kubernetes/charts/traefik-chart-values.yaml -n apps
 
