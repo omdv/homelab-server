5d1151160d4c5247ee0e3f910b697db6623c4caa
Oleg Medvedev <omdv@fastmail.com>
2022-01-23 21:23:25 -0600

    adding postgresql and related vault secrets

--- configure.sh
+++ configure.sh
@@ -267,6 +267,12 @@ generate_cluster_secrets() {
     kubectl exec -n vault vault-0 -- vault kv put kv/secret/wireguard name=my-wireguard-secret
     kubectl exec -n vault vault-0 -- vault kv patch kv/secret/wireguard "VAULT_WIREGUARD_COUNTRY"="$VAULT_WIREGUARD_COUNTRY"
     kubectl exec -n vault vault-0 -- vault kv patch kv/secret/wireguard "VAULT_WIREGUARD_CONFIG"="$(cat $WIREGUARD_CONFIG_FILE)"
+
+    # initialize secret @ secret/postgres
+    kubectl exec -n vault vault-0 -- vault kv put kv/secret/postgres name=my-dns-secret
+    for var in "${!VAULT_POSTGRES@}"; do
+        kubectl exec -n vault vault-0 -- vault kv patch kv/secret/postgres "$var"="$(echo -n ${!var})"
+    done
 }
 
 verify_argo() {
--- provision/terraform/cloudflare/main.tf
+++ provision/terraform/cloudflare/main.tf
@@ -100,8 +100,8 @@ resource "cloudflare_record" "root" {
   ttl     = 1
 }
 
-resource "cloudflare_record" "hajimari" {
-  name    = "hajimari"
+resource "cloudflare_record" "whoami" {
+  name    = "whoami"
   zone_id = lookup(data.cloudflare_zones.domain.zones[0], "id")
   value   = "ipv4.${data.sops_file.cloudflare_secrets.data["cloudflare_domain"]}"
   proxied = true
