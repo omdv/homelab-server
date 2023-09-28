94102d7f6d966f97c5905ebe65b7aa6fda416947
omdv <omdv@protonmail.com>
2023-09-27 21:35:47 -0500

    updated icloudpd deployment

--- configure.sh
+++ configure.sh
@@ -280,7 +280,10 @@ generate_cluster_secrets() {
 
     # initialize secret @ secret/icloudpd
     kubectl exec -n vault vault-0 -- vault kv put kv/secret/icloudpd name=my-icloudpd-secret
-    kubectl exec -n vault vault-0 -- vault kv patch kv/secret/icloudpd "VAULT_ICLOUDPD_APPLE_USER_OM"="$VAULT_ICLOUDPD_APPLE_USER_OM"
+    kubectl exec -n vault vault-0 -- vault kv patch kv/secret/icloudpd "VAULT_ICLOUDPD_USER_OM"="$VAULT_ICLOUDPD_USER_OM"
+    kubectl exec -n vault vault-0 -- vault kv patch kv/secret/icloudpd "VAULT_ICLOUDPD_TELEGRAM_TOKEN"="$VAULT_TELEGRAM_TOKEN"
+    kubectl exec -n vault vault-0 -- vault kv patch kv/secret/icloudpd "VAULT_ICLOUDPD_TELEGRAM_CHAT_ID"="$VAULT_TELEGRAM_CHAT_ID"
+
 
     # initialize secret @ secret/cloudflare
     kubectl exec -n vault vault-0 -- vault kv put kv/secret/cloudflare name=my-cloudflare-secret
@@ -296,7 +299,6 @@ generate_cluster_secrets() {
     kubectl exec -n vault vault-0 -- vault kv put kv/secret/ibkr name=my-ibkr-secret
     kubectl exec -n vault vault-0 -- vault kv patch kv/secret/ibkr "VAULT_IBKR_USER_ID"="$VAULT_TWS_USER_ID"
     kubectl exec -n vault vault-0 -- vault kv patch kv/secret/ibkr "VAULT_IBKR_PASSWORD"="$VAULT_TWS_PASSWORD"
-
 }
 
 
