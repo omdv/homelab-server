28d3e0dc9033bb4e7106e032ad9b191e5b2ff9a1
Oleg Medvedev <omdv@protonmail.com>
2024-08-02 19:31:43 -0500

    nix dev shell

---
+++ flake.nix
@@ -0,0 +1,18 @@
+{
+  description = "Homelab environment";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+  outputs = { self, nixpkgs }:
+    let
+      pkgs = nixpkgs.legacyPackages.x86_64-linux;
+    in
+    {
+      devShells.x86_64-linux.default = pkgs.mkShell {
+        nativeBuildInputs = with pkgs; [
+          pre-commit
+          helm
+        ];
+      };
+    };
+}
