5df5f1bfb537b81515fe94ed4fdc5fa8b7d1419a
Oleg Medvedev <omdv@protonmail.com>
2024-12-28 19:27:04 -0600

    add ibkr passwords to vault

--- configure.sh
+++ configure.sh
@@ -286,10 +286,10 @@ generate_cluster_secrets() {
         kubectl exec -n vault vault-0 -- vault kv patch kv/secret/cloudflare "$var"="$(echo -n ${!var})"
     done
 
-    # # initialize secret @ secret/ibkr
-    # kubectl exec -n vault vault-0 -- vault kv put kv/secret/ibkr name=my-ibkr-secret
-    # kubectl exec -n vault vault-0 -- vault kv patch kv/secret/ibkr "VAULT_IBKR_USER_ID"="$VAULT_TWS_USER_ID"
-    # kubectl exec -n vault vault-0 -- vault kv patch kv/secret/ibkr "VAULT_IBKR_PASSWORD"="$VAULT_TWS_PASSWORD"
+    # initialize secret @ secret/ibkr
+    kubectl exec -n vault vault-0 -- vault kv put kv/secret/ibkr name=my-ibkr-secret
+    kubectl exec -n vault vault-0 -- vault kv patch kv/secret/ibkr "VAULT_IBKR_USER_ID"="$VAULT_IBKR_LIVE_USER_ID"
+    kubectl exec -n vault vault-0 -- vault kv patch kv/secret/ibkr "VAULT_IBKR_PASSWORD"="$VAULT_IBKR_LIVE_PASSWORD"
 
     # # initialize secret @ secret/tailscale
     # kubectl exec -n vault vault-0 -- vault kv put kv/secret/tailscale name=my-tailscale-secret
